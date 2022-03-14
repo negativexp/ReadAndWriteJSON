5643853596056a8d6d84a2ea4dec0c090da86d39
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-03-14 09:52:19 +0100

    Add files via upload

---
+++ ReadandWriteJSON/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ReadandWriteJSON
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<JSONdata> _JSONDATA = new List<JSONdata>();
+        string finalJSON;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _JSONDATA.Add(new JSONdata()
+            {
+                Name = textBox1.Text,
+                Address = textBox2.Text,
+                Number = Convert.ToInt32(textBox3.Text)
+            });
+            finalJSON = JsonConvert.SerializeObject(_JSONDATA.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = finalJSON;
+        }
+    }
+}
---
+++ ReadandWriteJSON/JSONdata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadandWriteJSON
+{
+    public class JSONdata
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+
+    }
+}
